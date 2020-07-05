0d58fb3adce6380aa7879281596e91a80650e2c1
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-07-05 18:05:57 +0200

    Move StationsAPI into new file

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,19 +1,14 @@
+import { createRouter } from "@peregrine/koa-with-decorators"
+import dotenv from "dotenv"
 import Koa, { Context, Next } from "koa"
 import bodyParser from "koa-bodyparser"
-import { OVgoStaticAPI } from "./data-access/ovgostatic-api"
-import { NsApi } from "./data-access/ns-api"
-import { transformNsDeparture } from "./transformations/departure"
 // import { loadDisruptionsLegacy } from "./disruptions-legacy"
 import { ApiCacheManager } from "./data-access/ApiCacheManager"
-import { searchStations, searchStation } from "./searchStations"
-import { HttpGet, Path, createRouter, ApiController } from "@peregrine/koa-with-decorators"
-import dotenv from "dotenv"
+import { NsApi } from "./data-access/ns-api"
+import { OVgoStaticAPI } from "./data-access/ovgostatic-api"
+import { StationsAPI } from "./StationsAPI"
 
 if (process.env.NS_API_KEY === undefined) dotenv.config()
-
-async function sleep(timeInMilliSeconds: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, timeInMilliSeconds))
-}
 
 /** Utility function that wraps a callback function into a Promise. */
 async function promisify<R>(func: (cb: (err?: Error) => void) => R): Promise<R> {
@@ -25,59 +20,6 @@ async function promisify<R>(func: (cb: (err?: Error) => void) => R): Promise<R> 
     })
 }
 
-function getLanguage(_: Context): "en" | "nl" {
-    return "nl"
-}
-
-@ApiController("/api/v0")
-class StationsAPI {
-    public constructor(private readonly data: ApiCacheManager) {}
-
-    @HttpGet
-    @Path("/stations.json")
-    public async getAllStations(ctx: Context): Promise<void> {
-        // TODO: cache time of 60 * 60 * 24 * 5
-        const { q = null } = ctx.query
-        ctx.response.status = 200
-        ctx.response.body = q !== null && typeof q === "string" ? await searchStations(this.data, q, false) : await this.data.getStations()
-    }
-
-    @HttpGet
-    @Path("/stations/:id.json")
-    public async getStationById(ctx: Context): Promise<void> {
-        // TODO: cache time of 90
-        const stationCode = parseInt(ctx.params.id)
-
-        const stations = await this.data.getStations()
-        const station = stations.find(it => it.id === stationCode) ?? null
-
-        ctx.response.status = station === null ? 404 : 200
-        if (station !== null) ctx.response.body = station
-    }
-
-    @HttpGet
-    @Path("/stations/:id/departures.json")
-    public async getDeparturesForStationById(ctx: Context): Promise<void> {
-        // TODO: cache time of 90
-        const language = getLanguage(ctx)
-        const stationId = ctx.params.id.replace("%20", " ")
-
-        const uicCode = isNaN(parseInt(stationId)) ? (await searchStation(this.data, stationId))!.id : parseInt(stationId)
-
-        const nsDepartures = (await this.data.getDepartures(uicCode, language)).filter((_, i) => i < 12)
-
-        const departures = []
-
-        for (const departure of nsDepartures) {
-            await sleep(75)
-            departures.push(await transformNsDeparture(this.data, departure, language))
-        }
-
-        ctx.response.status = 200
-        ctx.response.body = await Promise.all(departures)
-    }
-}
-
 async function main(data: ApiCacheManager): Promise<void> {
     const koaApp = new Koa()
     koaApp.use(bodyParser())
@@ -87,11 +29,7 @@ async function main(data: ApiCacheManager): Promise<void> {
     koaApp.use(stationsRoute.routes())
     koaApp.use(stationsRoute.allowedMethods())
 
-    /* const v1ApiRouter = new Router({
-        prefix: "/api/v1"
-    }) */
-
-    // TODO: legacy loadDisruptionsLegacy
+    // TODO: loadDisruptionsLegacy
 
     // Remove default response body, catch all errors.
     koaApp.use(async (ctx: Context, next: Next) => {
@@ -111,7 +49,7 @@ async function main(data: ApiCacheManager): Promise<void> {
 
     await promisify((cb) => koaApp.listen(port, cb))
 
-    console.log(`Server is running on ${port}`)
+    console.log(`Server is running on port ${port}`)
 }
 
 main(new ApiCacheManager(new NsApi(undefined, process.env.NS_API_KEY), OVgoStaticAPI)).catch(console.error)
